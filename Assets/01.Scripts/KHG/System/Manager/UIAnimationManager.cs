de3a5d37a4a45a4aeafea324e0dfb7b1a217a458
1_ruf <gyeol0411@naver.com>
2024-12-18 19:14:23 +0900

    .add StagePass&Stage2

--- Assets/01.Scripts/KHG/System/Manager/UIAnimationManager.cs
+++ Assets/01.Scripts/KHG/System/Manager/UIAnimationManager.cs
@@ -4,9 +4,13 @@ public class UIAnimationManager : MonoBehaviour
 {
     [SerializeField] private Animator _animatorBackground;
     [SerializeField] private Animator _animatorUI;
+    [SerializeField] private Animator _animatorStagepass;
     public void PlayerAnim(string animName)
     {
-        if(_animatorBackground.GetBool(animName)) _animatorBackground.Play(animName);
-        else _animatorUI.Play(animName);
+        _animatorBackground.Play(animName);
+        _animatorUI.Play(animName);
+        _animatorStagepass.Play(animName);
+        //if(_animatorBackground.GetBool(animName)) _animatorBackground.Play(animName);
+        //else _animatorUI.Play(animName);
     }
 }
