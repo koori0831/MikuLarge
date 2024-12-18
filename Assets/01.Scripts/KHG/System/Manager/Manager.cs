5187e4f119574974d31d123b06c98e2557cb9c43
1_ruf <gyeol0411@naver.com>
2024-12-18 15:40:11 +0900

    .add shopSystem

--- Assets/01.Scripts/KHG/System/Manager/Manager.cs
+++ Assets/01.Scripts/KHG/System/Manager/Manager.cs
@@ -5,4 +5,7 @@ public class Manager : MonoBehaviour
     public MapManager_K MapManager_K;
     public CameraManager_K CameraManager_K;
     public ResourceManager ResourceManager;
+
+    [Header("UI")]
+    public ResourceUI ResourceUI;
 }
