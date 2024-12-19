cff73fc4dab9847866bdb70be7edb6fd266a1300
1_ruf <gyeol0411@naver.com>
2024-12-19 12:24:43 +0900

    .add RoomManager

---
+++ Assets/01.Scripts/KHG/System/Manager/RoomManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    
+    public bool DoorStatus { get; set; }/// <summary> 문 현재상태
+                                        ///     True이면 문이(방 상태가)열려있는 상태, False이면 닫힘
+                                        /// </summary>
+}
