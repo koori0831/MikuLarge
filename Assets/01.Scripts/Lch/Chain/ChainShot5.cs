f7447140410dd89b9a92b2b3d2d27c9e1183b764
koori0831 <fuyunokoori0831@gmail.com>
2024-12-20 00:33:00 +0900

    ADD : NewGuns!

--- Assets/01.Scripts/Lch/Chain/ChainShot5.cs
+++ Assets/01.Scripts/Lch/Chain/ChainShot5.cs
@@ -24,6 +24,20 @@ public class ChainShot5 : Entity
 
     public bool isEnd = false;
 
+    private void OnEnable()
+    {
+        base.Awake();
+        Camera mainCamera = Camera.main;
+
+        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
+
+        float cameraHeight = 2f * mainCamera.orthographicSize;
+        float cameraWidth = cameraHeight * mainCamera.aspect;
+
+        boxCollider.size = new Vector2(cameraWidth, cameraHeight);
+
+        transform.position = mainCamera.transform.position;
+    }
 
     private void Start()
     {
