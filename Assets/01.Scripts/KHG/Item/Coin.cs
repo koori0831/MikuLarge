3c03cfd168224029356e59f1d1b0fbbf1ffbe05e
1_ruf <gyeol0411@naver.com>
2024-12-19 12:25:18 +0900

    .edit NULL

--- Assets/01.Scripts/KHG/Item/Coin.cs
+++ Assets/01.Scripts/KHG/Item/Coin.cs
@@ -1,10 +1,20 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 
 public class Coin : MonoBehaviour, ICollectable
 {
+    private ParticleSystem _coinParticle;
+
+    private bool _particleCool;
     public void Collect()
     {
         CoinCollected();
+    }
+
+    private void Awake()
+    {
+        _coinParticle = GetComponent<ParticleSystem>();
     }
 
     private void CoinCollected()
@@ -12,5 +22,27 @@ public class Coin : MonoBehaviour, ICollectable
         Manager.manager.ResourceManager.Coin++;
         Manager.manager.ResourceUI.SetCoin();
         Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 7)
+        {
+            ParticleEmmit();
+        }
+    }
+
+    private void ParticleEmmit()
+    {
+        if (_particleCool) return;
+        _coinParticle.Play();
+        StartCoroutine(Cool(0.5f));
+    }
+
+    private IEnumerator Cool(float time)
+    {
+        _particleCool = true;
+        yield return new WaitForSeconds(time);
+        _particleCool = false;
     }
 }
