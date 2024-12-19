41e603d6bf68f986c1cff4cc391495af26853ea7
dlckdgh0414 <ci5976547@gmail.com>
2024-12-19 12:12:49 +0900

    feat : MapPrefabs

--- Assets/01.Scripts/Lch/State/Leviathan/LeviathenDeadState.cs
+++ Assets/01.Scripts/Lch/State/Leviathan/LeviathenDeadState.cs
@@ -7,6 +7,7 @@ public class LeviathenDeadState : EntityState
     private readonly int _deadLayer = LayerMask.NameToLayer("DeadBoss");
     private SpriteRenderer _spriteRenderer;
     private EntityMover _mover;
+    private int _spawnItemCount;
     public LeviathenDeadState(Entity entity, AnimParamSO animParam) : base(entity, animParam)
     {
         _leviathan = entity as Leviathan;
@@ -18,6 +19,7 @@ public class LeviathenDeadState : EntityState
     {
         base.Enter();
         _leviathan.gameObject.layer = _deadLayer;
+        _spawnItemCount = Random.Range(0, _leviathan.ItemList.DropItemList.Count);
     }
 
     public override void Update()
@@ -25,6 +27,7 @@ public class LeviathenDeadState : EntityState
         base.Update();
         if (_isTriggerCall)
         {
+            GameObject.Instantiate(_leviathan.ItemList.DropItemList[_spawnItemCount], _leviathan.transform.position, Quaternion.identity);
             _renderer.enabled = false;
             _mover.enabled = false;
             _spriteRenderer.color = Color.gray;
