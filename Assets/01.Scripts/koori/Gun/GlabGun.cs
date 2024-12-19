df2ae3000f70048429569ed57796184bd8ab3670
1_ruf <gyeol0411@naver.com>
2024-12-20 02:27:34 +0900

    feat : Item

--- Assets/01.Scripts/koori/Gun/GlabGun.cs
+++ Assets/01.Scripts/koori/Gun/GlabGun.cs
@@ -1,11 +1,36 @@
+using System;
 using UnityEngine;
 
 public class GlabGun : MonoBehaviour, IInteractable
 {
     [SerializeField] private GameObject gunPreFab;
+    [SerializeField] private ItemSO _itemSO;
+    private Player _player;
+    public bool IsShop;
     public void Interact(Player player)
     {
-        player.Hands.PickUpGun(gunPreFab);
+
+        _player = player;
+
+        if (Manager.manager.ResourceManager.Coin >= _itemSO.Cost && IsShop)
+        {
+            Manager.manager.ResourceManager.Coin -= _itemSO.Cost;
+            Manager.manager.ResourceUI.SetCoin();
+            GetItem();
+            Destroy(gameObject);
+        }
+
+        if (!IsShop)
+        {
+            player.Hands.PickUpGun(gunPreFab);
+            Destroy(gameObject);
+        }
+       
+    }
+
+    private void GetItem()
+    {
+        _player.Hands.PickUpGun(gunPreFab);
         Destroy(gameObject);
     }
 }
